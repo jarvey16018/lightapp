7d37da924c4fbb3dd4b4f9c3b99bee61df3639c1
jarvey16018 <jarvey16018@me.com>
2024-04-08 11:25:59 -0600

    Initial Commit

---
+++ lightapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lightapp
+//
+//  Created by Jason Harvey on 4/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
