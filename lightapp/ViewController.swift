5166d104fbb5d765c48284b1078372f700bcca03
jarvey16018 <jarvey16018@me.com>
2024-04-10 17:38:18 -0600

    Finished the app, the user just needs to tapp the screen to turn the light on and off.

--- lightapp/ViewController.swift
+++ lightapp/ViewController.swift
@@ -7,23 +7,26 @@
 
 import UIKit
 
-var lightOn = true
+
 
 class ViewController: UIViewController {
-
+    
+    
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
 
+    fileprivate func updateUI() {
+            view.backgroundColor = lightOn ? .white : .black
+    }
+    
     @IBAction func buttonPressed(_ sender: Any) {
         lightOn.toggle()
-        if lightOn {
-            view.backgroundColor = .white
-        } else {
-            view.backgroundColor = .black
-        }
+        updateUI()
     }
 }
 
